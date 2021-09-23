2c002c80ae7085009049bec58280365a414ec808
syafiraanbar <syafiraanbar@gmail.com>
2021-09-23 11:00:54 +0700

    menambahkan Contoh1.php

---
+++ Contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1?";
+        echo"Nama saya Syafira Anbar Roswita
+            Saya tinggal di Jalan Ciakar Awipari
+            Olah raga yang saya sukai adalah 
+            Renang
+            Nomor Handphone 082115711830";
+    }
+}
